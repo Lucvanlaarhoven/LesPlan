08b99e23a376d5b4388617a6cec73e3968f0cea2
Lucvanlaarhoven <lucvanlaarhoven@hotmail.com>
2025-04-16 13:33:14 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import Factuurgenerator from './components/Factuurgenerator';
+import Lesplanning from './components/Lesplanning';
+import SocialPost from './components/SocialPost';
+import ReviewRequest from './components/ReviewRequest';
+
+function App() {
+  return (
+    <div>
+      <h1>LesPlan</h1>
+      <Factuurgenerator />
+      <Lesplanning />
+      <SocialPost />
+      <ReviewRequest />
+    </div>
+  );
+}
+
+export default App;
+    
---
+++ ReviewRequest.js
@@ -0,0 +1,26 @@
+
+import React, { useState } from 'react';
+
+export default function ReviewRequest() {
+  const [studentName, setStudentName] = useState('');
+  const [reviewText, setReviewText] = useState('');
+
+  const generateReviewRequest = () => {
+    const result = `Hi ${studentName}, goed gedaan met je rijlessen! Als je een momentje hebt, zouden we het waarderen als je een korte review wilt achterlaten over je ervaring met LesPlan. Dank je wel!`;
+    setReviewText(result);
+  };
+
+  return (
+    <div>
+      <h3>Reviewverzoek Generator</h3>
+      <input
+        placeholder="Naam leerling"
+        value={studentName}
+        onChange={(e) => setStudentName(e.target.value)}
+      />
+      <button onClick={generateReviewRequest}>Genereer reviewbericht</button>
+      <textarea rows={4} value={reviewText} readOnly />
+    </div>
+  );
+}
+    
